6d6b3cfdb4010220c70d1429910ec311143fcd51
Litterial <yungice12@gmail.com>
2019-03-07 12:49:13 -0600

    cw

--- cwApp/forms.py
+++ cwApp/forms.py
@@ -1,21 +1,21 @@
 from django import forms
 from .models import HeroApplication
 
-MoneyPower=(('1','Rich'),('2','Super Powers'))
-Side=(('1','Good'),('2','Kinda good'),('3','Neutral'),('4','A little evil'),('5','Evil'))
-Abilities=(('1','Flight'),('2','Speed'),('3','Invisibility'),('4','Telekenetics'),('5','Healing'),('6','Other'))
+MoneyPower=(('1','Rich'),('2','Super Powers'))   #choices for whether the applicant is rich or has superpowers
+Side=(('1','Good'),('2','Kinda good'),('3','Neutral'),('4','A little evil'),('5','Evil'))  # choices for which side the applicant is on
+Abilities=(('1','Flight'),('2','Speed'),('3','Invisibility'),('4','Telekinetics'),('5','Healing'),('6','Other'),('7','None'),)  #choices for applicant's powers
 class HeroForm(forms.ModelForm):
     class Meta:
         model=HeroApplication
         fields='__all__'
-        labels={
+        labels={   #labels to display what will statement/question will show up for each selected field
             'city':"City or origin",
             'powers':'Are you rich or do you have Super Powers?',
-            'power_type':'If you have Super Powers, which ones?',
+            'power_type':'If you have Super Powers, which ones? ',
             'alignment':'Which of the following are you?',
             'example':'Give us three examples of when you used your super hero abilities.'
         }
-        widgets={
+        widgets={   #widgets as to what the user will see
             'powers':forms.RadioSelect(choices=MoneyPower),
             'alignment':forms.RadioSelect(choices=Side),
             'example':forms.Textarea,
--- cwApp/views.py
+++ cwApp/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render,HttpResponse,redirect
 from .forms import HeroForm,HeroApplication
 # Create your views here.
-def index(request):
+def index(request):  #sends user to welcome page
     return render(request,'cwApp/index.html',)
-def application(request):
+def application(request): #application
     apply=HeroForm(request.POST or None)
     context={
         'form':apply,
     }
-    if request.method == 'POST':
-        if apply.is_valid():
+    if request.method == 'POST':   #if this is a post method
+        if apply.is_valid():  #if the application is valid
             apply.save()
             return redirect('conformation')
 
-        else:
+        else:   #if there are errors
             apply=HeroForm(request.POST)
             context={
                 'form':apply,
+                'errors':apply.errors
             }
-            return render(request,'cwApp/application.html',context)
+            return render(request,'cwApp/application.html',context) #return back to application will filled our information
     return render(request,'cwApp/application.html',context)
 
-def conformation(request):
+def conformation(request): #sends user to conformation page
     return render(request,'cwApp/conformation.html',)
